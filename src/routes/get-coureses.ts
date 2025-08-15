c9296f4e9856895b9d517444f9d1fd4104f645a5
HulitosCode <heltonmatimbe8@gmail.com>
2025-08-15 13:53:37 +0200

    feat: adding filter and ordenation

--- src/routes/get-coureses.ts
+++ src/routes/get-coureses.ts
@@ -2,6 +2,7 @@ import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { db } from "../database/client.ts";
 import { courses } from "../database/schema.ts";
 import z from "zod";
+import { ilike, asc } from "drizzle-orm"
 
 export const getCoursesRoute: FastifyPluginAsyncZod = async (app) => {
   app.get(
@@ -10,6 +11,10 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (app) => {
       schema: {
         tags: ["courses"],
         summary: "Get all courses",
+        querystring: z.object({
+          search: z.string().optional(),
+          orderBy: z.enum(['id', 'title']).optional().default('id')
+        }),
         response: {
           200: z.object({
             courses: z.array(
@@ -23,13 +28,20 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (app) => {
       },
     },
     async (request, reply) => {
+      const { search, orderBy } = request.query
+
       // No Select escolhemos quais dados queremos trazer, passando os objectos dentro,ou sem objecto para trazer todos dados
       const result = await db
         .select({
           id: courses.id,
           title: courses.title,
         })
-        .from(courses);
+        .from(courses)
+        .orderBy(asc(courses[orderBy]))
+        .where(
+          search ? ilike(courses.title, `%${search}%`): undefined
+        )
+  
       return reply.send({ courses: result });
     },
   );
