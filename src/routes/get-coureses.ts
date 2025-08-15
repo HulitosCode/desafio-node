72e54df089c4e8b19353b1be83634f213cd340e6
HulitosCode <heltonmatimbe8@gmail.com>
2025-08-15 15:00:19 +0200

    feat: improving my function

--- src/routes/get-coureses.ts
+++ src/routes/get-coureses.ts
@@ -2,7 +2,7 @@ import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { db } from "../database/client.ts";
 import { courses } from "../database/schema.ts";
 import z from "zod";
-import { ilike, asc } from "drizzle-orm"
+import { ilike, asc, SQL, and, count } from "drizzle-orm"
 
 export const getCoursesRoute: FastifyPluginAsyncZod = async (app) => {
   app.get(
@@ -13,7 +13,8 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (app) => {
         summary: "Get all courses",
         querystring: z.object({
           search: z.string().optional(),
-          orderBy: z.enum(['id', 'title']).optional().default('id')
+          orderBy: z.enum(['id', 'title']).optional().default('id'),
+          page: z.coerce.number().optional().default(1)
         }),
         response: {
           200: z.object({
@@ -23,26 +24,37 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (app) => {
                 title: z.string(),
               }),
             ),
+             total: z.number()
           }),
         },
       },
     },
     async (request, reply) => {
-      const { search, orderBy } = request.query
+      const { search, orderBy, page } = request.query
 
-      // No Select escolhemos quais dados queremos trazer, passando os objectos dentro,ou sem objecto para trazer todos dados
-      const result = await db
+      const conditions: SQL[] = []
+
+      if (search) {
+        conditions.push(ilike(courses.title, `%${search}%`))
+      }
+
+      const [result, total] = await Promise.all([
+        db
         .select({
           id: courses.id,
           title: courses.title,
         })
         .from(courses)
         .orderBy(asc(courses[orderBy]))
-        .where(
-          search ? ilike(courses.title, `%${search}%`): undefined
-        )
-  
-      return reply.send({ courses: result });
+        .offset((page - 1) * 2) // Pula o numero de paginas para buscar os registos
+        .limit(2)
+        .where(and(...conditions))
+        .groupBy(courses.id),
+      db.$count(courses, and(...conditions))
+        
+      ])
+        
+      return reply.send({ courses: result, total });
     },
   );
 };
