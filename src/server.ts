96b069be874dcba1a93190f1b57e2317c9907434
HulitosCode <heltonmatimbe8@gmail.com>
2025-08-25 14:03:20 +0200

    fix: ...

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-// import { app } from "./app.ts";
+import { app } from "./app.ts";
 
-// app.listen({
-//   port: Number(process.env.PORT) || 3333,
-//   host: "0.0.0.0"
-// }).then(() => {
-//   console.log("HTTP server running!");
-// });
+app.listen({
+  port: Number(process.env.PORT) || 3333,
+  host: "0.0.0.0"
+}).then(() => {
+  console.log("HTTP server running!");
+});
