75654f92407454bc937ef61db4651b77ddb38c22
HulitosCode <heltonmatimbe8@gmail.com>
2025-08-25 12:07:39 +0200

    fix: ...

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { app } from "./app.ts";
 
-app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
+
+app.listen({ port: Number (process.env.PORT) || 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP server running!");
 });
