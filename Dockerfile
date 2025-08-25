94e3ee8c490a03da85c46ed82cc426e431562b35
HulitosCode <heltonmatimbe8@gmail.com>
2025-08-25 12:14:12 +0200

    fix: ...

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:22-alpine
 WORKDIR /app
 
-COPY package*.json ./
+COPY . ./
 RUN npm ci
 
 COPY . .
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { app } from "./app.ts";
 
-
-app.listen({ port: Number (process.env.PORT) || 3333, host: "0.0.0.0" }).then(() => {
+app.listen({
+  port: Number(process.env.PORT) || 3333,
+  host: "0.0.0.0"
+}).then(() => {
   console.log("HTTP server running!");
 });
